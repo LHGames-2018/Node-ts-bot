b5d18553bbb0e2ca7fe6226b2f0634edcd6760ee
Gabriel Beaudry <gabriel.bdry@gmail.com>
2018-10-04 20:29:10 -0400

    Fix

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { GameInfo } from '../interfaces';
+import { GameInfo, Player } from '../interfaces';
 import { Map } from '../map';
 import { Bot } from '../bot';
 
@@ -7,23 +7,28 @@ module Route {
 
     export class Index {
         private bot: Bot;
+
         public ping(res: Response) {
             res.send("I am alive!");
         }
 
-        public index(req: Request, res: Response, next: NextFunction) {
+        public index = (req: Request, res: Response, next: NextFunction) => {
             if (!this.bot) {
                 this.bot = new Bot();
             }
 
-            console.log(req.body);
             const mapData = JSON.parse(req.body.data) as GameInfo;
             const map = new Map(mapData.CustomSerializedMap, mapData.xMin, mapData.yMin);
 
+            mapData.Player = Object.assign(new Player(), mapData.Player);
+            mapData.OtherPlayers.forEach(player => {
+                player = Object.assign(new Player(), player);
+            });
+
             this.bot.beforeTurn(mapData.Player);
+
             const action = this.bot.executeTurn(map, mapData.OtherPlayers);
             this.bot.afterTurn();
-
             res.send(action);
         }
     }
